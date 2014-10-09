f7973b32074fe98675cd0f7556ed1b51015486da
李翌文 <johnnyeven@gmail.com>
2014-10-09 23:21:08 +0800

    1

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(session({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
+app.use(require('./routes/dogecoin'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

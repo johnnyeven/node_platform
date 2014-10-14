31876267c150fe82e0df7b01850e6798952cc91f
johnnyeven_work <johnnyeven@gmail.com>
2014-10-14 14:21:41 +0800

    1

---
+++ cronjobs/check_new_charge.js
@@ -0,0 +1,15 @@
+/**
+ * check_new_charge.js
+ * Author: johnnyeven
+ * Time: 2014-10-14
+ * Comment: 每1分钟检查新的充值记录
+ */
+
+var config = require('../config');
+var mongodb = require('../modules/db');
+var dogecoin = require('node-dogecoin')({
+	host: config.dogecoind.rpchost,
+	port: config.dogecoind.rpcport,
+	user: config.dogecoind.rpcuser,
+	pass: config.dogecoind.rpcpassword
+});
